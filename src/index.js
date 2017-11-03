fc2f6867ecb3138674af4198b43c68e9b9e1892e
juanjocerero <elquefaltaba@gmail.com>
2017-11-03 15:54:32 +0100

    this aint shit

--- src/index.js
+++ src/index.js
@@ -1,18 +1,60 @@
 import * as csv from 'd3-dsv'
 import * as maps from '@google/maps'
+import stringify from 'csv-stringify'
 import * as fs from 'fs'
 import * as path from 'path'
 
 const load = file => fs.readFileSync(path.join(__dirname, `../data/${file}`), { encoding: 'utf8' })
 
-const rawData = csv.csvParse(load('data.csv'))
-const calles = rawData.map(d => `${d.calle_proper}, Granada, Spain`)
+const exportAsCsv = (data, fileName) => {
+  stringify(data, (error, output) => {
+    if (error) {
+      throw new Error(error)
+    }
+    try {
+      console.log(`Saving ${fileName}.csv`)
+      fs.writeFileSync(
+        path.join(
+          __dirname, 
+          `/../output/${fileName}.csv`
+        ),
+        output
+      )
+    } catch (error) {
+      throw new Error(error)
+    }
+  })
+}
+
+const rawData = csv.csvParse(load('full_data.csv'))
+
+const calles = rawData.map(d => ({
+  searchString: `${d.calle_proper}, Granada, Spain`,
+  calle: d.calle
+}))
+
+exportAsCsv(Array.from(calles.map(d => d.searchString)), 'gc')
 
 const mapsClient = maps.createClient({
   key: 'AIzaSyAG7y6UxZE1PkDPYXKJ0z-zvZgf9ghOiEs'
 })
 
-mapsClient.geocode({ address: calles[0] }, (err, response) => {
-  if (err) throw new Error(err)
-  console.log(response.json.results)
-})
+// const geoCode = calle => {
+//   return new Promise((resolve, reject) => {
+//     mapsClient.geocode({ address: calle.searchString }, (err, response) => {
+//       if (err) reject(err)
+//       resolve({
+//         calle: calle.searchString,
+//         calle_old: calle.calle,
+//         latitude: response.json.results[0].geometry.location.lat,
+//         longitude: response.json.results[0].geometry.location.lng
+//       })
+//     })
+//   })
+// }
+
+// Promise.all(calles.map(c => geoCode(c)))
+// .then(values => {
+//   console.log(values)
+//   exportAsCsv(values, '1')
+// })
